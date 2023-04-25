1c07195ee835d570c286b3913a2abf0ab8293212
ATSmusz <65310062+asmusz-adobe@users.noreply.github.com>
2023-04-25 16:41:42 -0700

    Create upload_n_send.py

---
+++ upload_n_send.py
@@ -0,0 +1,73 @@
+"""
+Uploads pdf document to EchoSign and returns its transientDocumentId
+Sends agreement using transient to single signer.
+
+"""
+import requests
+import json
+from random import *
+
+thisToken = '3AAABLbl****** ACCESS TOKEN OR INTEGRATION KEY HERE ******3EfgELfnS1rzdwxL' # your access token or integration key
+fpath = './fileToSend.pdf' # path to file
+tname = 'star_wars.pdf' # transient doc name
+myShard = 'na4' # the "shard" your account is on
+senderEmail = 'sender@example.com' # email address of sender or token owner
+recipt_email = 'recipient-email@example.com' # email address of recipient
+rand_number = randint(1, 100)
+
+
+def upl_trans(api_token, file_path, docName, ashard, sender):
+    url = f"https://secure.{ashard}.adobesign.com/api/rest/v6/transientDocuments"
+    payload={'File-Name': docName}
+    files=[
+        ('File',('empty.pdf',open(file_path,'rb'),'application/pdf'))
+        ]
+    headers = {
+            'x-api-user': f'email:{sender}',
+            'Authorization': f'Bearer {api_token}'
+        }
+    return requests.request("POST", url, headers=headers, data=payload, files=files).json().get('transientDocumentId')
+
+def send_last_trans_to_one_signer(api_token, shard, trans_id, ag_name, sender, rcpt_email):
+    url = f"https://secure.{shard}.adobesign.com/api/rest/v6/agreements"
+    payload = json.dumps({
+        "fileInfos": [
+                {
+                "transientDocumentId": trans_id
+                }
+            ],
+        "name": ag_name,
+        "participantSetsInfo": [
+            {
+                "memberInfos": [
+                        {
+                            "email": rcpt_email
+                        }
+                    ],
+                "order": 1,
+                "role": "SIGNER"
+            }
+        ],
+        "message": "Plese sign this from us.",
+        "signatureType": "ESIGN",
+        "externalId": {
+                        "id": rand_number
+        },
+        "state": "IN_PROCESS"
+    })
+    headers = {
+                'x-api-user': f'email:{sender}',
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {api_token}'
+            }
+    response = requests.request("POST", url, headers=headers, data=payload).json().get('id')
+    return response
+
+
+uploadtransID = upl_trans(thisToken, fpath, tname, myShard, senderEmail)
+
+print(f'Got transient upload with ID:\n\n {uploadtransID}\n\n Now sending Agreement to {recipt_email}\n\n')
+
+new_ag_id = send_last_trans_to_one_signer(thisToken, myShard, uploadtransID, f"New agreement Test{rand_number}", senderEmail, recipt_email)
+
+print(f'Agreement with ID: {new_ag_id} was sent to {recipt_email}. \n\nPlease have {recipt_email} check their inBox!')
