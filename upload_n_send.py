129a82fc1cfe354967630be61aa0981726ec407f
ATSmusz <65310062+asmusz-adobe@users.noreply.github.com>
2023-04-25 17:06:44 -0700

    Update upload_n_send.py

--- upload_n_send.py
+++ upload_n_send.py
@@ -7,14 +7,13 @@ import requests
 import json
 from random import *
 
-thisToken = '3AAABLbl****** ACCESS TOKEN OR INTEGRATION KEY HERE ******3EfgELfnS1rzdwxL' # your access token or integration key
-fpath = './fileToSend.pdf' # path to file
-tname = 'star_wars.pdf' # transient doc name
+thisToken = '3AAABLblqZ****** ACCESS TOKEN OR INTEGRATION KEY HERE ******LfnS1rzdwxL' # your access token or integration key
+fpath = './some_file_to_upload.pdf' # path to file
+tname = 'my_document_name.pdf' # transient doc name with extension
 myShard = 'na4' # the "shard" your account is on
-senderEmail = 'sender@example.com' # email address of sender or token owner
-recipt_email = 'recipient-email@example.com' # email address of recipient
+senderEmail = 'sender_email@example.com' # email address of sender or token owner
+recipt_email = 'recipient_email@example.com' # email address of recipient
 rand_number = randint(1, 100)
-
 
 def upl_trans(api_token, file_path, docName, ashard, sender):
     url = f"https://secure.{ashard}.adobesign.com/api/rest/v6/transientDocuments"
@@ -24,7 +23,8 @@ def upl_trans(api_token, file_path, docName, ashard, sender):
         ]
     headers = {
             'x-api-user': f'email:{sender}',
-            'Authorization': f'Bearer {api_token}'
+            'Authorization': f'Bearer {api_token}',
+            'User-Agent': f'MyApplicationName_to_help_with_troubleshooting_{rand_number}' # Please include a user agent string
         }
     return requests.request("POST", url, headers=headers, data=payload, files=files).json().get('transientDocumentId')
 
@@ -58,7 +58,8 @@ def send_last_trans_to_one_signer(api_token, shard, trans_id, ag_name, sender, r
     headers = {
                 'x-api-user': f'email:{sender}',
                 'Content-Type': 'application/json',
-                'Authorization': f'Bearer {api_token}'
+                'Authorization': f'Bearer {api_token}',
+                'User-Agent': f'MyApplicationName_to_help_with_troubleshooting_{rand_number}' # Please include a user agent string
             }
     response = requests.request("POST", url, headers=headers, data=payload).json().get('id')
     return response
@@ -68,6 +69,7 @@ uploadtransID = upl_trans(thisToken, fpath, tname, myShard, senderEmail)
 
 print(f'Got transient upload with ID:\n\n {uploadtransID}\n\n Now sending Agreement to {recipt_email}\n\n')
 
-new_ag_id = send_last_trans_to_one_signer(thisToken, myShard, uploadtransID, f"New agreement Test{rand_number}", senderEmail, recipt_email)
+new_ag_id = send_last_trans_to_one_signer(thisToken, myShard, uploadtransID, f"New agreement Test {rand_number}", senderEmail, recipt_email)
 
-print(f'Agreement with ID: {new_ag_id} was sent to {recipt_email}. \n\nPlease have {recipt_email} check their inBox!')
+print(f'Agreement \n\n"New agreement Test {rand_number}"\n\nwith ID: \n\n    {new_ag_id}\n\n    was sent to {recipt_email}. \n\nPlease have {recipt_email} check their inBox!\n\n')
+
